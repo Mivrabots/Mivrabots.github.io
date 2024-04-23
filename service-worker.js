e1953a4fac1e9c5d9a30581b361de0c19214ce8c
mivra <156608502+Mivrabots@users.noreply.github.com>
2024-04-23 13:03:07 -0500

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+// service-worker.js
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/rps.css',
+        '/rps.js',
+        '/bot.gif,
+        // Add more URLs of your website assets to cache here
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
